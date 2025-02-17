963c2852dc2c57c5b0119f4ec1fef549e38eeec8
maryna.semash@gmail.com <1010Mse!>
2025-02-17 17:30:47 +0100

    add pom and specs, requests, models, enums, config

---
+++ src/test/java/com/example/teamcity/api/DummyTest.java
@@ -0,0 +1,20 @@
+package com.example.teamcity.api;
+
+import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.spec.Specifications;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class DummyTest extends BaseApiTest {
+    @Test
+    public void userShouldBeAbleToGetAllProjects() {
+        RestAssured
+                .given()
+                .spec(Specifications.getSpec()
+                        .authSpec(User.builder()
+                                .username("admin") // Fixed field name
+                                .password("admin")
+                                .build()))
+                .get("/app/rest/projects");
+    }
+}
